0c058d17172e49e23afe7d0071f53a64f6058391
Anastasiawangyx <1192146619@qq.com>
2020-10-13 22:06:24 +0800

    demo3

---
+++ demo3/cmake-build-debug/CMakeFiles/learn1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn1.dir/glad.c.o"
+  "CMakeFiles/learn1.dir/main.cpp.o"
+  "learn1"
+  "learn1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/learn1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
